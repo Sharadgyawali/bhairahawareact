2ea5c0f540352882e845ba4a9d78500a37e8ab54
Sharad Gyawali <your_email@example.com>
2025-07-08 07:40:39 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Home from "./Pages/home";
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello guys
+      <Home></Home>
+    </h1>
+  )
+}
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,7 @@
+function Home(){
+    return(<div>
+        tera bhai banega coder
+        
+    </div>)
+}
+export default Home
